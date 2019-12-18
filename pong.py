cb5dce0ccccdccbe1d292e54042502fae7d3251e
Klemen Kotar <klemen@cs.washington.edu>
2019-12-17 18:07:15 -0800

    Fixed bug in test code

--- pong.py
+++ pong.py
@@ -38,20 +38,18 @@ for e in range(NUM_EPOCHS):
         out = transfomer(seq, tgt)
         # compute the 3 different loss functions
         emb_loss = F.binary_cross_entropy(torch.sigmoid(out[:,:,:512]), torch.sigmoid(tgt[:,:,:512]))
-        print("idx:", idx, " --- out shape:", out[:,:,512:518].squeeze().shape, " --- tgt shape:", torch.argmax(tgt[:,:,512:518].squeeze(), dim=1).shape)
         action_loss = F.cross_entropy(out[:,:,512:518].squeeze(), torch.argmax(tgt[:,:,512:518].squeeze(), dim=1))
         value_loss = F.mse_loss(out[:,:,518], tgt[:,:,518])
         loss = emb_loss + action_loss + value_loss
         loss.backward()     
         optim.step()
         train_losses.append(loss.item())
-        print(loss.item())
     for idx in generate_batch_indexes(900000, 1000000, SEQ_LEN * BATCH_SIZE):
         seq, tgt = make_batch(idx, SEQ_LEN, batch_size=BATCH_SIZE)
         out = transfomer(seq, tgt)
         # compute the 3 different loss functions
         emb_loss = F.binary_cross_entropy(torch.sigmoid(out[:,:,:512]), torch.sigmoid(tgt[:,:,:512]))
-        action_loss = F.cross_entropy(out[:,:,512:518], torch.argmax(tgt[:,:,512:518]))
+        action_loss = F.cross_entropy(out[:,:,512:518].squeeze(), torch.argmax(tgt[:,:,512:518].squeeze(), dim=1))
         value_loss = F.mse_loss(out[:,:,518], tgt[:,:,518])
         loss = emb_loss + action_loss + value_loss
         loss.backward()
