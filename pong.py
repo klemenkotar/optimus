Fixed incorrect softmax loss

pong.py
@@ -7,7 +7,7 @@ import numpy as np
 from tqdm import tqdm
 
 FILE = np.load('data/embeddings.npy', mmap_mode='r')
-BATCH_SIZE = 10
+BATCH_SIZE = 50
 SEQ_LEN = 100
 NUM_EPOCHS = 100
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -48,7 +48,7 @@ for e in range(NUM_EPOCHS):
         out = transfomer(seq, tgt)
         # compute the 3 different loss functions
         emb_loss = F.binary_cross_entropy(torch.sigmoid(out[:,:,:512]), torch.sigmoid(tgt[:,:,:512]))
-        action_loss = F.cross_entropy(out[:,:,512:518].squeeze(), torch.argmax(tgt[:,:,512:518].squeeze(), dim=1))
+        action_loss = F.cross_entropy(out[:,:,512:518], torch.argmax(tgt[:,:,512:518], dim=2))
         value_loss = F.mse_loss(out[:,:,518], tgt[:,:,518])
         loss = emb_loss + action_loss + value_loss
         loss.backward()     
@@ -60,7 +60,7 @@ for e in range(NUM_EPOCHS):
         out = transfomer(seq, tgt)
         # compute the 3 different loss functions
         emb_loss = F.binary_cross_entropy(torch.sigmoid(out[:,:,:512]), torch.sigmoid(tgt[:,:,:512]))
-        action_loss = F.cross_entropy(out[:,:,512:518].squeeze(), torch.argmax(tgt[:,:,512:518].squeeze(), dim=1))
+        action_loss = F.cross_entropy(out[:,:,512:518], torch.argmax(tgt[:,:,512:518], dim=2))
         value_loss = F.mse_loss(out[:,:,518], tgt[:,:,518])
         test_emb_loss.append(emb_loss.item())
         test_action_loss.append(action_loss.item())