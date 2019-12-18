6245fcc7efcbe18ff8f90553cc1d88843b875599
Klemen Kotar <klemen@cs.washington.edu>
2019-12-18 00:37:55 -0800

    Fixed binary crossentropy loss by joining batches and sequences into the same dimension

--- pong.py
+++ pong.py
@@ -48,7 +48,7 @@ for e in range(NUM_EPOCHS):
         out = transfomer(seq, tgt)
         # compute the 3 different loss functions
         emb_loss = F.binary_cross_entropy(torch.sigmoid(out[:,:,:512]), torch.sigmoid(tgt[:,:,:512]))
-        action_loss = F.cross_entropy(out[:,:,512:518], torch.argmax(tgt[:,:,512:518], dim=2))
+        action_loss = F.cross_entropy(out[:,:,512:518], torch.argmax(tgt[:,:,512:518], dim=1))
         value_loss = F.mse_loss(out[:,:,518], tgt[:,:,518])
         loss = emb_loss + action_loss + value_loss
         loss.backward()     
@@ -60,7 +60,7 @@ for e in range(NUM_EPOCHS):
         out = transfomer(seq, tgt)
         # compute the 3 different loss functions
         emb_loss = F.binary_cross_entropy(torch.sigmoid(out[:,:,:512]), torch.sigmoid(tgt[:,:,:512]))
-        action_loss = F.cross_entropy(out[:,:,512:518], torch.argmax(tgt[:,:,512:518], dim=2))
+        action_loss = F.cross_entropy(out[:,:,512:518], torch.argmax(tgt[:,:,512:518], dim=1))
         value_loss = F.mse_loss(out[:,:,518], tgt[:,:,518])
         test_emb_loss.append(emb_loss.item())
         test_action_loss.append(action_loss.item())
