dcb80ac86e2f0e124e10f8db70fb575e11000fe3
Klemen Kotar <klemen@cs.washington.edu>
2019-12-18 16:14:15 -0800

    Moved new model to gpu

--- pong.py
+++ pong.py
@@ -33,7 +33,7 @@ def generate_batch_indexes(start, stop, step):
 
 # transfomer = nn.Transformer(d_model=528, nhead=16, num_encoder_layers=12).to(DEVICE)
 encoder_layers = nn.TransformerEncoderLayer(528, 16, 528, dropout=0.4)
-transfomer = nn.TransformerEncoder(encoder_layers, 12)
+transfomer = nn.TransformerEncoder(encoder_layers, 12).to(DEVICE)
 optim = torch.optim.Adam(transfomer.parameters())
 
 for e in range(NUM_EPOCHS):
