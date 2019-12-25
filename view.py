3b2f8c9e65e0820a12c0d46475440ec622e675ab
Klemen Kotar <klemen@cs.washington.edu>
2019-12-25 02:11:24 -0800

    Added cloning of tensor to fix a bug

--- view.py
+++ view.py
@@ -337,7 +337,7 @@ for i in range(20):
     print("ROLLING OUT FRAME", i+1)
     out = model(DATA, ACTIONS)
     out = torch.argmax(out.permute(0,2,3,1), dim=3)
-    DATA[:-1] = DATA[1:]
+    DATA[:-1] = DATA[1:].clone()
     DATA[-1] = out[-1]
     out = out[-1]
     # plt.figure(i)
