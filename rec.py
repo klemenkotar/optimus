877f0458877ee4392de9104e6634922499ebe723
Klemen Kotar <klemen@cs.washington.edu>
2019-12-26 11:54:46 -0800

    Fixed bug by cloning tensor

--- rec.py
+++ rec.py
@@ -127,7 +127,7 @@ class Reconstruction(nn.Module):
         # Pass sequence through transformer
         for _ in range(1):
             new_seq = self.transformer(seq, seq)
-            seq[:-1] = seq[1:]
+            seq[:-1] = seq[1:].clone()
             seq[-1] = new_seq[-1]
         seq[-1] = act[-1]
         trans_out = seq.squeeze()
