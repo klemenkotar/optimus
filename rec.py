a8b84a64e1e9aab6c441ac2f0a7292244fca661d
Klemen Kotar <klemen@cs.washington.edu>
2019-12-26 11:47:37 -0800

    Set up training run where the original embedings are preserved during the sequence rollouts, and added embedding multiplication back to the embedding layers

--- rec.py
+++ rec.py
@@ -126,7 +126,9 @@ class Reconstruction(nn.Module):
 
         # Pass sequence through transformer
         for _ in range(1):
-            seq = self.transformer(seq, seq)
+            new_seq = self.transformer(seq, seq)
+            seq[:-1] = seq[1:]
+            seq[-1] = new_seq[-1]
         seq[-1] = act[-1]
         trans_out = seq.squeeze()
 
@@ -134,10 +136,10 @@ class Reconstruction(nn.Module):
         deconv_in = torch.zeros((x.shape[0], 128, 2, 2)).to(DEVICE)
         for i in range(x.shape[0]):
             idx = (i * 5)
-            deconv_in[i, :, 0, 0] = trans_out[idx] #* trans_out[idx+4]
-            deconv_in[i, :, 0, 1] = trans_out[idx+1] #* trans_out[idx+4]
-            deconv_in[i, :, 1, 0] = trans_out[idx+2] #* trans_out[idx+4]
-            deconv_in[i, :, 1, 1] = trans_out[idx+3] #* trans_out[idx+4]
+            deconv_in[i, :, 0, 0] = trans_out[idx] * trans_out[idx+4]
+            deconv_in[i, :, 0, 1] = trans_out[idx+1] * trans_out[idx+4]
+            deconv_in[i, :, 1, 0] = trans_out[idx+2] * trans_out[idx+4]
+            deconv_in[i, :, 1, 1] = trans_out[idx+3] * trans_out[idx+4]
 
         # Deconvolve embeddings
         # out = self.deconv(deconv_in)
