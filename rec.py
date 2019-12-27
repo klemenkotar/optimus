56bc0ceb4a7c24a05014de470768f2da017e0a2d
Klemen Kotar <klemen@cs.washington.edu>
2019-12-27 15:53:36 -0800

    Set up run with a lot of embedding training, like 2000 epochs

--- rec.py
+++ rec.py
@@ -15,7 +15,7 @@ SEQ_LEN = 100
 NUM_STEPS = 20000
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 PATH = 'models/rec-res-2x2.pt'
-LR = 1e-4
+LR = 1e-5
 WEIGHT_DECAY = 0.0
 
 DATA = torch.zeros(NUM_STEPS, 84, 84)
@@ -418,8 +418,8 @@ while step < NUM_STEPS:
         model.optim.step()
         train_losses.append(loss.item())
     print("Loss:", np.mean(train_losses))
-    # print("Training in the Embedding Space")
-    # model.train_embeddings(step)
+    print("Training in the Embedding Space")
+    model.train_embeddings(step, epochs=2000)
 
 seq, tgt, act = make_batch(idx, SEQ_LEN)
 out = model(seq, act)
